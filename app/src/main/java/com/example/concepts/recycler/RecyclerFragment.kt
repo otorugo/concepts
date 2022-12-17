ba3aaa8f4f4107cbc0e7338218fecd069d9cf1c5
Victor <victorsilva698@gmail.com>
2022-12-17 12:42:33 -0300

    init a projects that assemble studied conceps

---
+++ app/src/main/java/com/example/concepts/recycler/RecyclerFragment.kt
@@ -0,0 +1,30 @@
+package com.example.concepts.recycler
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.concepts.R
+
+
+class RecyclerFragment : Fragment(R.layout.fragment_recycler) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recycler, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.skip_recycler).setOnClickListener {
+            findNavController().navigate(R.id.action_recyclerFragment_to_internetDataFragment)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/concepts/recycler/models/Live.kt
@@ -0,0 +1,4 @@
+package com.example.concepts.recycler.models
+
+//data class Live {
+//}
